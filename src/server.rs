0f94639ad9d83b1b01a809bc4e44837c43dc27a0
Audun Halland <audun.halland@gmail.com>
2021-04-29 00:53:15 +0200

    fix: graphql subscription

--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,6 @@
 use std::convert::Infallible;
 
+use async_graphql_warp::graphql_subscription;
 use warp::http::StatusCode;
 use warp::{http::Response as HttpResponse, Filter, Rejection};
 
@@ -12,6 +13,8 @@ use crate::schema::mutation::Mutation;
 use crate::schema::query::Query;
 use crate::schema::subscription::Subscription;
 
+const SERVER_PORT: u16 = 8000;
+
 ///
 /// Start a web server providing the /graphql endpoint plus a playground.
 ///
@@ -19,28 +22,38 @@ use crate::schema::subscription::Subscription;
 /// The server is a future that never completes.
 ///
 pub async fn serve(pg_pool: sqlx::PgPool) {
-    let event_bus = EventBus::new();
-    let schema = Schema::build(Query, Mutation, Subscription).finish();
+    let schema = Schema::build(Query, Mutation, Subscription)
+        .data(Repository::new(pg_pool))
+        .data(EventBus::new())
+        .finish();
 
     let graphql_post = warp::path!("graphql")
         .and(warp::post())
         // The followig filter has an Extract type, and provides the first parameter to `execute_graphql`:
-        .and(async_graphql_warp::graphql(schema))
-        // warp::Filter::map also adds an Extract type, and this provides the second parameter to `execute_graphql`:
-        .and(warp::any().map(move || pg_pool.clone()))
-        // etc, third argument:
-        .and(warp::any().map(move || event_bus.clone()))
-        .and_then(execute_graphql);
+        .and(async_graphql_warp::graphql(schema.clone()))
+        .and_then(
+            |(schema, request): (Schema<_, _, _>, async_graphql::Request)| async move {
+                let response = schema.execute(request).await;
+
+                // graphql never 'fails' at the HTTP status-code level:
+                Ok::<_, Infallible>(async_graphql_warp::Response::from(response))
+            },
+        );
 
     let graphql_playground = warp::path::end().and(warp::get()).map(|| {
         HttpResponse::builder()
             .header("content-type", "text/html")
-            .body(playground_source(GraphQLPlaygroundConfig::new("/graphql")))
+            .body(playground_source(
+                GraphQLPlaygroundConfig::new("/graphql")
+                    .subscription_endpoint(&format!("ws://localhost:{}", SERVER_PORT)),
+            ))
     });
 
     let routes = graphql_post
+        .or(graphql_subscription(schema))
         .or(graphql_playground)
         .recover(|err: Rejection| async move {
+            println!("rejection: {:?}", err);
             if let Some(async_graphql_warp::BadRequest(err)) = err.find() {
                 return Ok::<_, Infallible>(warp::reply::with_status(
                     err.to_string(),
@@ -54,23 +67,5 @@ pub async fn serve(pg_pool: sqlx::PgPool) {
             ))
         });
 
-    warp::serve(routes).run(([0, 0, 0, 0], 8000)).await;
-}
-
-async fn execute_graphql(
-    (schema, mut request): (
-        Schema<Query, Mutation, Subscription>,
-        async_graphql::Request,
-    ),
-    pg_pool: sqlx::PgPool,
-    event_bus: EventBus,
-) -> Result<async_graphql_warp::Response, Infallible> {
-    // Set up the GraphQL context:
-    request = request.data(Repository::new(pg_pool));
-    request = request.data(event_bus);
-
-    let response = schema.execute(request).await;
-
-    // graphql never 'fails' at the HTTP status-code level:
-    Ok(async_graphql_warp::Response::from(response))
+    warp::serve(routes).run(([0, 0, 0, 0], SERVER_PORT)).await;
 }
